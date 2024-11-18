bcf4447d57cb3b1ef98092ebcd443a642e2a1958
Sumbal Haroon <tsanamina8@gmail.com>
2024-11-18 07:41:59 -0800

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -27,7 +27,6 @@ st.markdown("""
     .result {
         font-size: 24px;
         color: #4CAF58;
-        text-align: center;
         margin-top: 20px;
     }
     </style>
@@ -65,56 +64,4 @@ if st.button("Analyze Sentiment"):
     else:
         st.write("Please enter text for analysis.")
 
-
-
-
-
-
-
-
-
-
-
-
-
-# # streamlit_app.py
-
-# import streamlit as st
-# import pandas as pd
-# import numpy as np
-# import tensorflow as tf
-# from tensorflow.keras.preprocessing.sequence import pad_sequences
-# import joblib
-
-# # Load the model
-# model = tf.keras.models.load_model('rnn_sentiment_model5.h5')
  
-
-# # Load the tokenizer
-# tokenizer = joblib.load('tokenizer2.joblib')
-
-
-# # Streamlit app title and introduction
-# st.title("Sentiment Analysis with RNN")
-# st.write("This app predicts the sentiment (positive or negative) of a given text using a trained Simple RNN model.")
-
-# # Text input from the user
-# user_input = st.text_area("Enter your text here:")
-
-# # Preprocess and predict sentiment
-# if st.button("Analyze Sentiment"):
-#     if user_input:
-#         # Preprocess the input text
-#         input_sequence = tokenizer.texts_to_sequences([user_input])
-#         input_padded = pad_sequences(input_sequence, maxlen=300, padding='post', truncating='post')
-
-#         # Make prediction
-#         prediction = model.predict(input_padded)[0][0]
-
-#         # Display the result
-#         sentiment = "Positive" if prediction > 0.5 else "Negative"
-#         confidence = prediction if sentiment == "Positive" else 1 - prediction
-#         st.write(f"**Sentiment**: {sentiment}")
-#         st.write(f"**Confidence**: {confidence:.2f}")
-#     else:
-#         st.write("Please enter text for analysis.")
